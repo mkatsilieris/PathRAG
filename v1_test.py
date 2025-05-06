066813df165b9a9d32a644f4326caac8335ea8be
Michael Katsilieris <mkatsili@netcompany.com>
2025-05-06 13:49:35 +0300

    Refactor v1_test.py to use azure_openai_complete and set working directory; add .gitignore for environment and cache files

--- v1_test.py
+++ v1_test.py
@@ -1,13 +1,14 @@
 import os
 from PathRAG import PathRAG, QueryParam
-from PathRAG.llm import gpt_4o_mini_complete
+from PathRAG.llm import azure_openai_complete
 
-WORKING_DIR = ""
 
-api_key=""
-os.environ["OPENAI_API_KEY"] = api_key
-base_url="https://api.openai.com/v1"
-os.environ["OPENAI_API_BASE"]=base_url
+WORKING_DIR = r"C:\Users\mkatsili\projects\PathRAG\content_repository"
+
+# api_key=""
+# os.environ["OPENAI_API_KEY"] = api_key
+# base_url="https://api.openai.com/v1"
+# os.environ["OPENAI_API_BASE"]=base_url
 
 
 if not os.path.exists(WORKING_DIR):
@@ -15,12 +16,12 @@ if not os.path.exists(WORKING_DIR):
 
 rag = PathRAG(
     working_dir=WORKING_DIR,
-    llm_model_func=gpt_4o_mini_complete,  
+    llm_model_func=azure_openai_complete,  
 )
 
-data_file=""
-question=""
-with open(data_file) as f:
+data_file = r"C:\Users\mkatsili\projects\PathRAG\content_repository\J495_001_2023-ΚΑΤΑΧΩΡΗΣΗ ΑΙΤΗΜΑΤΟΣ ΚΑΤΑΝΑΛΩΤΙΚΟΥ ΔΑΝΕΙΟΥ ΜΕΣΩ ΚΑΤΑΣΤΗΜΑΤΟΣ_WF.md"
+question="βηματα διαδικασιας Καταναλωτικού Δανείου"
+with open(data_file, encoding='utf-8') as f:
     rag.insert(f.read())
 
 print(rag.query(question, param=QueryParam(mode="hybrid")))
